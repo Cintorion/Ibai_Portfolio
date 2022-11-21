f121f64640fb7d19f47b3bbc68aaffbadb7c06e0
Cintorion <114365125+Cintorion@users.noreply.github.com>
2022-11-21 12:08:13 +0000

    Add files via upload

---
+++ Task_1 CP/Task_1 version 1.0.py
@@ -0,0 +1,88 @@
+
+import random
+
+print("Password Generator 1.0")
+print("Choose a range from 1-10 passwords")
+Pass_words = ["gloves", "computer", "curly", "bell", "baseball", "thunder", "worldwide", "beatbox", "bacon", "shoes", "kilogram", "pen", "salt", "bottle", "cup", "headphones", "caramel"]
+password_times = int(input("How many password are needed?: "))
+
+if password_times == 1:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+
+elif password_times == 2:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+elif password_times == 3:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+elif password_times == 4:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+elif password_times == 5:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+elif password_times == 6:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+elif password_times == 7:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+elif password_times == 8:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+elif password_times == 9:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+elif password_times == 10:
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+    print(str(random.choice(Pass_words) + random.choice(Pass_words)))
+
+else:
+    print("Choose a range from 1-10, try again")
+
+
